75f53a547549c1c19c9e753a788ed990486b50b0
YJ <yunyun3599@gmail.com>
2020-11-17 19:57:48 +0900

    fitts' law with target

---
+++ Fitt's Law with target/Assets/Script/Laser.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+using DetectTrigger;
+
+public class Laser : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabPinch;
+
+
+    private LineRenderer laser;        // 레이저
+    private RaycastHit Collided_object; // 충돌된 객체
+    //private GameObject currentObject;   // 가장 최근에 충돌한 객체를 저장하기 위한 객체
+
+    public float raycastDistance = 100f; // 레이저 포인터 감지 거리
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 스크립트가 포함된 객체에 라인 렌더러라는 컴포넌트를 넣고있다.
+        laser = this.gameObject.AddComponent<LineRenderer>();
+
+        // 라인이 가지개될 색상 표현
+        Material material = new Material(Shader.Find("Standard"));
+        material.color = new Color(0, 195, 255, 0.5f);
+        laser.material = material;
+        // 레이저의 꼭지점은 2개가 필요 더 많이 넣으면 곡선도 표현 할 수 있다.
+        laser.positionCount = 2;
+        // 레이저 굵기 표현
+        laser.startWidth = 0.01f;
+        laser.endWidth = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laser.SetPosition(0, transform.position); // 첫번째 시작점 위치
+                                                  // 업데이트에 넣어 줌으로써, 플레이어가 이동하면 이동을 따라가게 된다.
+                                                  //  선 만들기(충돌 감지를 위한)
+        Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.green, 0.5f);
+
+        // 충돌 감지 시
+        if (Physics.Raycast(transform.position, transform.forward, out Collided_object, raycastDistance))
+        {
+            laser.SetPosition(1, Collided_object.point);
+
+            // 충돌 객체의 태그가 Sphere인 경우
+            if (Collided_object.collider.gameObject.CompareTag("Sphere"))
+            {
+                laser.material.color = new Color(255, 0, 0, 0.5f);
+                trigger.on = 1;
+            }
+            else
+            {
+                laser.material.color = new Color(0, 195, 255, 0.5f);
+                trigger.on = 0;
+            }
+        }
+
+        else
+        {
+            // 레이저에 감지된 것이 없기 때문에 레이저 초기 설정 길이만큼 길게 만든다.
+            laser.SetPosition(1, transform.position + (transform.forward * raycastDistance));
+
+
+        }
+
+    }
+
+}
---
+++ Fitt's Law with target/Assets/Script/Target.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using DetectTrigger;
+using System.Diagnostics;
+
+
+namespace DetectTrigger
+{
+    class trigger
+    {
+        public static int on = 0;
+    }
+}
+
+public class Target : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabPinch;
+    
+    public int status = 0;
+
+    public Transform obj;
+    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+    public int[] angle = { 0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345, 360 };
+    int theta;
+    float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theta = angle[Random.Range(0, 24)];
+        distance = Random.Range(1, 3) * 1.5f;
+        sw.Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (status==0 && trigger.on==1 && GetTrigger())
+        {
+            transform.position = new Vector3((float)2.25+distance*Mathf.Sin(theta), (float)3.73+ distance*Mathf.Cos(theta), 0.11f);
+            theta = angle[Random.Range(0, 24)];
+            distance = Random.Range(1, 3) * 1.5f;
+            trigger.on = 0;
+            status = 1;
+            sw.Start();
+        }
+        if(status==1 && trigger.on==1 && GetTrigger())
+        {
+            status = 0;
+            sw.Stop();
+            UnityEngine.Debug.Log("Time: "+sw.ElapsedMilliseconds / 1000.0F);
+            UnityEngine.Debug.Log("Angle: "+theta);
+            UnityEngine.Debug.Log("Distance :"+ distance);
+            transform.position = new Vector3(2.25f, 3.73f, 0.11f);
+            trigger.on = 0;
+            status = 0;
+            sw.Reset();
+        }
+    }
+
+    public bool GetTrigger()
+    {
+        return grabPinch.GetState(handType);
+    }
+}
